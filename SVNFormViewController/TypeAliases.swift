546afa41a0c481ea43d3c6ed08731fd8cbc40c9b
Aaron Bikis <aaron.bikis@gmail.com>
2017-09-19 12:51:01 -0700

    update dependencies

---
+++ SVNFormViewController/TypeAliases.swift
@@ -0,0 +1,11 @@
+//
+//  TypeAliases.swift
+//  SVNFormViewController
+//
+//  Created by Aaron Dean Bikis on 9/8/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+import Foundation
+
+public typealias Credentials = [SVNFormFieldType: String]
